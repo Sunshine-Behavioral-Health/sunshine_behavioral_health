5c6a9392e889c5c8bee4258f1c3a73ccc0858b9f
NaNathan13 <natewilson135@gmail.com>
2021-08-20 22:56:56 -0400

    TRAC-40: Removed tests

--- template-parts/content/shortcodes/a_b_contact_form.php
+++ template-parts/content/shortcodes/a_b_contact_form.php
@@ -1,17 +1,16 @@
-<?php 
+<?php
 
-$standardContactFormID = get_field('standard_contact_form_id', 'option');
+$standardContactFormID = '[gravityform id="' . get_field('standard_contact_form_id', 'option') . '" title="false" description="false"]';
 ?>
 
 
 <section class="a-b-test a-b-test--contact-form contact_cta_section contact_form_shortcode_section shortcode_section">
-    <h1><?= $standardContactFormID ?></h1>
     <div class="contact_form_cta_container contact_form_shortcode_container">
         <h4 class="contact_form_cta_headline">Take The First Step Towards Recovery</h4>
         <p>Talk to a Intake Coordinator</p>
 
         <div class="contact_form_cta_form_container contact_form_shortcode_cta_form_container">
-            <?php echo do_shortcode("[gravityform id='. $standardContactFormID . ' title='false' description='false']") ?>
+            <?= do_shortcode($standardContactFormID); ?>
         </div>
     </div>
 </section>
--- template-parts/content/shortcodes/a_b_insurance_form.php
+++ template-parts/content/shortcodes/a_b_insurance_form.php
@@ -1,15 +1,14 @@
-<?php 
+<?php
 
-$insuranceFormID = get_field('insurance_form_id', 'option');
+$insuranceFormID = '[gravityform id="' . get_field('standard_contact_form_id', 'option') . '" title="false" description="false"]';
 ?>
 
 
 <section class="a-b-test a-b-test--insurance-form contact_cta_section contact_form_shortcode_section shortcode_section">
-    <h1><?= $insuranceFormID ?></h1>
     <div class="contact_form_cta_container contact_form_shortcode_container">
         <h4 class="contact_form_cta_headline">Verify Your Insurnace</h4>
         <div class="contact_form_cta_form_container contact_form_shortcode_cta_form_container">
-            <?php echo do_shortcode("[gravityform id='. $insuranceFormID . ' title='false' description='false']") ?>
+            <?= do_shortcode($insuranceFormID); ?>
         </div>
     </div>
 </section>
