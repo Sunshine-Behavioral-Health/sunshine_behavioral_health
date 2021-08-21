ed0528e186c59c929a411ead984915ae67a198c7
NaNathan13 <natewilson135@gmail.com>
2021-08-20 22:52:28 -0400

    TRAC-40: Added tests

--- template-parts/content/shortcodes/a_b_contact_form.php
+++ template-parts/content/shortcodes/a_b_contact_form.php
@@ -5,6 +5,7 @@ $standardContactFormID = get_field('standard_contact_form_id', 'option');
 
 
 <section class="a-b-test a-b-test--contact-form contact_cta_section contact_form_shortcode_section shortcode_section">
+    <h1><?= $standardContactFormID ?></h1>
     <div class="contact_form_cta_container contact_form_shortcode_container">
         <h4 class="contact_form_cta_headline">Take The First Step Towards Recovery</h4>
         <p>Talk to a Intake Coordinator</p>
--- template-parts/content/shortcodes/a_b_insurance_form.php
+++ template-parts/content/shortcodes/a_b_insurance_form.php
@@ -5,6 +5,7 @@ $insuranceFormID = get_field('insurance_form_id', 'option');
 
 
 <section class="a-b-test a-b-test--insurance-form contact_cta_section contact_form_shortcode_section shortcode_section">
+    <h1><?= $insuranceFormID ?></h1>
     <div class="contact_form_cta_container contact_form_shortcode_container">
         <h4 class="contact_form_cta_headline">Verify Your Insurnace</h4>
         <div class="contact_form_cta_form_container contact_form_shortcode_cta_form_container">
