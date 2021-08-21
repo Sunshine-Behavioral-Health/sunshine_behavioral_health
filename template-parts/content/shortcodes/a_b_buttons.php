a98442588e67e2ba4e21b0c4f7b0724dcefb00e8
NaNathan13 <natewilson135@gmail.com>
2021-08-20 22:43:37 -0400

    TRAC-40: Created new repo because permissions were wonky

--- template-parts/content/shortcodes/a_b_buttons.php
+++ template-parts/content/shortcodes/a_b_buttons.php
@@ -1,4 +1,4 @@
-<section class="a-b-test a-b-test--buttons two_buttons_cta_section shortcode_section">
+<section class="a-b-test a-b-test--buttons extra-class two_buttons_cta_section shortcode_section">
     <div class="two_buttons_cta_container">
         <div class="shortcode_two_buttons_cta shortcode_two_buttons_first_cta">
             <a href="tel:949-276-2886" id="twoLongButtonCtaPhone" class="invocaNumber twoLongButtonCtaPhone" onclick="dataLayer.push({'event': 'phone_click', 'shortcode_type' : 'twoLongButtonCtaPhone'});">Talk with a treatment specialist 24/7 – Call 949-276-2886</a>
