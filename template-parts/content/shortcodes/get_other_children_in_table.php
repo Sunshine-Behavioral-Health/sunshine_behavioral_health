5696954a52e6efeebf002ec802c8ee2c7105520c
NaNathan13 <natewilson135@gmail.com>
2021-09-08 21:53:49 -0400

    DEV-162: Fixed css for get_other_children_in_table and updated svg

--- template-parts/content/shortcodes/get_other_children_in_table.php
+++ template-parts/content/shortcodes/get_other_children_in_table.php
@@ -21,12 +21,11 @@ $childrenPages = get_children()
         <div class="get-other-children-in-table--container m-r-20">
             <ul class="flex flex-wrap space-around">
                 <?php foreach ($childrenPages as $page) : ?>
-                    <li class="m-5 flex justify-center align-center"><a class="color-secondary" href="<?= $page->guid ?>"><?= $page->post_title ?></a></li>
+                    <li class="m-5 flex justify-center align-center"><a class="color-secondary text-center" href="<?= $page->guid ?>"><?= $page->post_title ?></a></li>
                 <?php endforeach; ?>
             </ul>
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill-opacity="1" d="M0,64L48,96C96,128,192,192,288,202.7C384,213,480,171,576,149.3C672,128,768,128,864,138.7C960,149,1056,171,1152,197.3C1248,224,1344,256,1392,272L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,96L48,117.3C96,139,192,181,288,213.3C384,245,480,267,576,266.7C672,267,768,245,864,229.3C960,213,1056,203,1152,213.3C1248,224,1344,256,1392,272L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+
     </div>
 </section>
